64bbc9c156683405eeb614778bae97ae4498c221
tak <njirtak@gmail.com>
2018-09-24 12:06:12 +0900

    feat: add new buzzler service

--- buzzler/buzzler.cpp
+++ buzzler/buzzler.cpp
@@ -1,118 +1,21 @@
 #include<buzzler.hpp>
 
-void buzzler::hello(account_name user)
+void buzzler_service::createuser(const account_name server,
+                                 const account_name account)
 {
-    print("Hello, ", name{user});
-}
-
-void buzzler::create(const account_name account,
-                     const string&      nickname,
-                     const string&      univ,
-                     const string&      major)
-{
-    require_auth(account);
+    require_auth(server);
 
     // check account on user_table
-    user_table users(_self, _self);
-    auto itr = users.find(account);
-    eosio_assert(itr == users.end(), "Account already has a user");
+    auto itr = user_table.find(account);
+    eosio_assert(itr == user_table.end(), "Account already has a user");
 
     // create user
-    users.emplace(account, [&](auto& p) {
+    user_table.emplace(account, [&](auto& p) {
         p.account    = account;
-        p.nickname   = nickname;
-        p.univ       = univ;
-        p.major      = major;
-        p.token      = 0;
+        p.buzz_token = 0;
         p.created_at = now();
     });
 
     // debug print
     print(name{account}, " user created");
-}
-
-void buzzler::update(const account_name account,
-                     const string&      nickname,
-                     const string&      univ,
-                     const string&      major,
-                     uint64_t           token)
-{
-    require_auth(account);
-
-    // check account on user_table
-    user_table users(_self, _self);
-    auto itr = users.find(account);
-    eosio_assert(itr != users.end(), "Account does not has a user");
-
-    // update user
-    users.modify(itr, account, [&](auto& p) {
-        p.nickname = nickname;
-        p.univ     = univ;
-        p.major    = major;
-        p.token    = token;
-    });
-
-    // debug print
-    print(name{account}, " modified");
-}
-
-void buzzler::remove(const account_name account)
-{
-    require_auth(account);
-
-    // check account on user_table
-    user_table users(_self, _self);
-    auto itr = users.find(account);
-    eosio_assert(itr != users.end(), "Account does not has a user");
-
-    // remove user
-    users.erase(itr);
-
-    // debug print
-    print(name{account}, " user removed");
-}
-
-void buzzler::getuser(const account_name account)
-{
-    user_table users(_self, _self);
-    auto itr = users.find(account);
-
-    eosio_assert(itr != users.end(), "Account does not has a user");
-
-    // print user
-    print("Account: ",  name{itr->account},    ", ");
-    print("Nickname: ", itr->nickname.c_str(), ", ");
-    print("Univ: ",     itr->univ.c_str(),     ", ");
-    print("Major: ",    itr->major.c_str(),    ", ");
-    print("Token: ",    itr->token);
-
-}
-
-void buzzler::bytoken(uint64_t to_find_token)
-{
-    user_table users(_self, _self);
-    auto token_index = users.get_index<N(token)>();
-    auto itr = token_index.find(to_find_token);
-
-    for (; itr != token_index.end() && itr->token == to_find_token; ++itr) {
-        // print user
-        print("Account: ",  name{itr->account},    ", ");
-        print("Nickname: ", itr->nickname.c_str(), ", ");
-        print("Univ: ",     itr->univ.c_str(),     ", ");
-        print("Major: ",    itr->major.c_str(),    ", ");
-        print("Token: ",    itr->token,            "\n");
-    }
-}
-
-void buzzler::rangetoken(uint64_t less, uint64_t over)
-{
-    user_table users(_self, _self);
-    auto token_index = users.get_index<N(token)>();
-
-    auto begin = token_index.lower_bound(less);
-    auto end   = token_index.lower_bound(over);
-
-    for_each(begin, end, [&](auto &p) {
-        print(name{p.account}, "'s token: ", p.token, "\n");
-    });
 }
--- buzzler/buzzler.hpp
+++ buzzler/buzzler.hpp
@@ -1,85 +1,33 @@
-#include <eosiolib/eosio.hpp>
-#include <eosiolib/print.hpp>
+#include <eosiolib/eosio.hpp> 
 #include <string>
 
-using namespace eosio;
-using std::string;
+using namespace eosio; 
+using namespace std;
 
-class buzzler: public eosio::contract {
+class buzzler_service: public eosio::contract {
 
-    public:
-        using contract::contract;
-
-        // @abi action
-        void hello(account_name user);
+    public: 
+        buzzler_service(account_name self): contract::contract(self),
+                                            user_table(self, self) {}
 
         // @abi action
-        void create(const account_name account,
-                    const string&      nickname,
-                    const string&      univ,
-                    const string&      major);
-
-        // @abi action
-        void update(const account_name account,
-                    const string&      nickname,
-                    const string&      univ,
-                    const string&      major,
-                    uint64_t           token);
-
-        // @abi action
-        void remove(const account_name account);
-
-        // @abi action
-        void getuser(const account_name account);
-
-        // @abi action
-        void bytoken(uint64_t token);
-
-        // @abi action
-        void rangetoken(uint64_t less, uint64_t over);
+        void createuser(const account_name server,
+                        const account_name account);
 
     private:
 
-        // @abi table users
+        // @abi table users i64
         struct user {
             account_name account;
-            string       nickname;
-            string       univ;
-            string       major;
-            uint64_t     token;
+            uint64_t     buzz_token;
             time         created_at;
 
-            account_name primary_key() const { return account; }
-            uint64_t     by_token() const { return token; }
-            EOSLIB_SERIALIZE(user, (account)(nickname)(univ)(major)(token)(created_at))
+            auto primary_key() const { return account; }
+
+            EOSLIB_SERIALIZE(user, (account)(buzz_token)(created_at))
         };
 
-        typedef eosio::multi_index<
-            N(users), user,
-            indexed_by< N(token), const_mem_fun<user, uint64_t, &user::by_token> >
-        > user_table;
-};
+        multi_index<N(users), user> user_table;
+ };
 
-EOSIO_ABI(buzzler, (hello)(create)(update)(remove)(getuser)(bytoken)(rangetoken))
-
-
-///////
-
-// account table
-// - eosaccount_name
-// - buzztoken
-// - created_at
-
-// - action
-//     - create account ->
-
-// post table
-// - post_hash
-// - amount(보상 토큰)
-// - 모두 require_auth -> server
-// - created_at
-
-// post_like
-// post_comment
-
-// inline
+EOSIO_ABI(buzzler_service, (createuser))
