9a884a9af12d2a4f28fe736bac5b4e55e0bb3e65
tak <njirtak@gmail.com>
2018-10-02 20:18:24 +0900

    feat: define comment table

--- buzzler/buzzler.hpp
+++ buzzler/buzzler.hpp
@@ -9,7 +9,8 @@ class buzzler_service: public eosio::contract {
     public: 
         buzzler_service(account_name self): contract::contract(self),
                                             user_table(self, self),
-                                            post_table(self, self) {}
+                                            post_table(self, self),
+                                            comment_table(self, self) {}
 
         /*
          *  user actions
@@ -75,11 +76,33 @@ class buzzler_service: public eosio::contract {
             EOSLIB_SERIALIZE(post, (id)(author)(post_hash)(buzz_amount)(like_count)(created_at))
         };
 
+        // @abi table posts
+        struct comment {
+            uint64_t     id;
+            uint64_t     parent_id;
+            uint64_t     post_id;
+            account_name author;
+            string       comment_hash; // comment 해쉬값(contents...)
+            uint32_t     buzz_amount;  // 보상 토근
+            uint32_t     like_count; 
+            time         created_at;
+
+            auto     primary_key()  const { return id; }
+            uint64_t by_parent_id() const { return parent_id; }
+            uint64_t by_post_id()   const { return post_id; }
+
+            EOSLIB_SERIALIZE(comment, (id)(parent_id)(post_id)(author)(comment_hash)(buzz_amount)(like_count)(created_at))
+        };
+
         // define tables
         multi_index<N(users), user> user_table;
         multi_index<N(posts), post,
                     indexed_by<N(author), const_mem_fun<post, uint64_t, &post::by_author>>
         > post_table;
+        multi_index<N(comments), comment,
+                    indexed_by<N(parentid), const_mem_fun<comment, uint64_t, &comment::by_parent_id>>,
+                    indexed_by<N(postid), const_mem_fun<comment, uint64_t, &comment::by_post_id>>
+        > comment_table;
  };
 
 EOSIO_ABI(buzzler_service, (createuser)(updatetoken)(writepost)(updatepost)(deletepost)(postbyid)(postsbyuser))
